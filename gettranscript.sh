99e981eca77fef6a19512e40d5a56af7ce28e1ed
pharath <phrth2@gmail.com>
2024-06-28 17:21:44 +0200

    add a subtitle to transcript formatter script

---
+++ gettranscript.sh
@@ -0,0 +1,21 @@
+filename="$0"
+srtfile="$1"
+transcriptfile="$2"
+
+if [ -z "${srtfile}" ]; then
+  echo "error: argument srtfile is missing"
+  echo " "
+  echo "usage: $filename srtfile.srt transcriptfile.txt"
+  echo " "
+  exit
+elif [ -z "${transcriptfile}" ]; then
+  echo "error: argument transcriptfile is missing"
+  echo " "
+  echo "usage: $filename srtfile.srt transcriptfile.txt"
+  echo " "
+  exit
+fi
+
+grep -v ^0 "$srtfile" | grep -v ^$ | awk 'NR % 2 == 0' >> "$transcriptfile"
+echo " "
+echo "wrote to $transcriptfile."
